fe01a524abfef48262e5bcfd03ec7c40cad737ae
argelcapela <argelcapeladossantos@gmail.com>
2022-01-10 00:06:10 +0100

    a

--- test/main.c
+++ test/main.c
@@ -5,7 +5,8 @@
  * COMPILE AND EXECUTE DIRECT OF THIS CURRENT DIRECTORY
    ALSO DELETE .GCH file
 
-gcc -D BUFFER_SIZE=2 ../src/get_next_line.h ../src/get_next_line_utils.c ../src/get_next_line.c main.c && rm ../src/*.gch && ./a.exe
+gcc -D BUFFER_SIZE=2 ../src/get_next_line.h ../src/get_next_line_utils.c ../src/get_next_line.c main.c && rm ../src/*.gch 
+&& ./a.exe|out
 
  */
 
